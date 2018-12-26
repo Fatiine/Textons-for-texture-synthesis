0e7f5588a6e6807719d189e9db0fd5dedf009907
Fatine BOUJNOUNI <fatine.boujnouni@gmail.Com>
2018-12-26 20:55:23 +0100

    Organize data, code and papers

--- code/Main.m
+++ code/Main.m
@@ -1,4 +1,4 @@
-a=imread('wall.png');
+a=imread('../data/wall.png');
 a=mean(a,3);
 l=Texton(a);
 l=uint8(l);
@@ -7,12 +7,21 @@ I = double(l);
 I=fftshift(I);
 I=normalize_image(I);
 %imagesc(I);colormap gray;
-
 %Afficher La matrice de covariance de a
 %imagesc(cov(a));colormap gray;
 
 %Afficher le composant périodique
 [C,s] = per_decomp_smooth(a);
 %C = normalize_image(C);
+
+% mydisp = @(x)log10(abs(fftshift(x))+1e-5);
+% clf;
+% subplot(1,2,1);
+% imagesc(mydisp(a));colormap gray;
+% title('Original');
+% subplot(1,2,2);
+% imagesc(mydisp(C));colormap gray;
+% title('Periodic layer');
+
 imagesc(C);colormap gray;
 
