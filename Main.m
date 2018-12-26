fb3cb53cd6bf1d3c9df9946bc3d4ea1d896069f2
Fatine BOUJNOUNI <fatine.boujnouni@gmail.Com>
2018-12-26 18:51:02 +0100

    periodic+smooth decomposition

--- Main.m
+++ Main.m
@@ -4,11 +4,15 @@ l=Texton(a);
 l=uint8(l);
 I = double(l);
 %Afficher Texton
-I=fftshift(I)
-I=normalize_image(I)
-imagesc(I);colormap gray;
+I=fftshift(I);
+I=normalize_image(I);
+%imagesc(I);colormap gray;
 
 %Afficher La matrice de covariance de a
-imagesc(cov(a));colormap gray;
+%imagesc(cov(a));colormap gray;
 
+%Afficher le composant périodique
+[C,s] = per_decomp_smooth(a);
+%C = normalize_image(C);
+imagesc(C);colormap gray;
 
---
+++ per_decomp_smooth.m
@@ -0,0 +1,15 @@
+function [p,s] = per_decomp_smooth(u)
+
+[M,N] = size(u); 
+u = double(u);
+X = 1:N; Y = 1:M;
+v = zeros(M,N);
+v(1,X)  = u(1,X)-u(M,X);
+v(M,X) = -v(1,X);
+v(Y,1 ) = v(Y,1 )+u(Y,1)-u(Y,N);
+v(Y,N) = v(Y,N)-u(Y,1)+u(Y,N);
+fx = repmat(cos(2.*pi*(X -1)/N),M,1);
+fy = repmat(cos(2.*pi*(Y'-1)/M),1,N);
+fx(1,1)=0.;   % avoid division by 0 in the line below
+s = real(ifft2(fft2(v)*0.5./(2.-fx-fy)));
+p = u-s;
