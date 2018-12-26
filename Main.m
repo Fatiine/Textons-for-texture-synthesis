c324f1f0b946599157a20bd30690e52d7192413d
DaoudH <44686864+DaoudH@users.noreply.github.com>
2018-12-26 17:33:35 +0100

    Update Main.m

--- Main.m
+++ Main.m
@@ -4,7 +4,10 @@ l=Texton(a);
 l=uint8(l);
 I = double(l);
 %Afficher Texton
+I=fftshift(I)
+I=normalize_image(I)
 imagesc(I);colormap gray;
+
 %Afficher La matrice de covariance de a
 imagesc(cov(a));colormap gray;
 
