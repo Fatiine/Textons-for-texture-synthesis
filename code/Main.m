2265eb37136d7f4ea89c2a3e1c463ba72c2b3adf
DaoudH <44686864+DaoudH@users.noreply.github.com>
2018-12-27 12:14:19 +0100

    Update Main.m
    Ajout du code de seuillage et synthèse de texture. cf 3.2 ++> article "A Compact Representation of Random Phase and
    Gaussian Texture"

--- code/Main.m
+++ code/Main.m
@@ -1,16 +1,45 @@
-a=imread('../data/wall.png');
+a=imread('wall.png');
+%Niveaux de gris
 a=mean(a,3);
+%Afficher l'image
+figure
+imagesc(a);colormap gray;
+title('Original')
+%Texton
 l=Texton(a);
 l=uint8(l);
 I = double(l);
-%Afficher Texton
 I=fftshift(I);
 I=normalize_image(I);
-%imagesc(I);colormap gray;
-%Afficher La matrice de covariance de a
-%imagesc(cov(a));colormap gray;
+%Afficher Texton
+figure
+imagesc(I);colormap gray;
+title('Texton')
+%Seuillage
+I1=I;
+I1(I1<200 & I1>1)=100;
+imagesc(I1);colormap gray;
+figure
+imagesc(I1);colormap gray;
+title('Texton thresholding')
+% Randomization
+[M,N]=size(I1)
+I2=100*ones(size(I1)*2);
+I2(1:M,1:N)=I1;
+A=fft2(I2);
+n=randn(size(I2));
+N=fft2(n);
+B=abs(A).*exp(i*angle(N));
+b=real(ifft2(B));
+figure
+imagesc(b);colormap gray;
+title(" synthesize texture after phase randomization")
 
-%Afficher le composant périodique
+
+%Afficher La matrice de covariance de a
+imagesc(cov(a));colormap gray;
+
+%Afficher le composant pÃ©riodique
 [C,s] = per_decomp_smooth(a);
 %C = normalize_image(C);
 
@@ -22,6 +51,6 @@ I=normalize_image(I);
 % subplot(1,2,2);
 % imagesc(mydisp(C));colormap gray;
 % title('Periodic layer');
-
+figure
 imagesc(C);colormap gray;
 
