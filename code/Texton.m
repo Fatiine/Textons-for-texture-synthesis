e5282ed810e2235762ec6cffd521d16d60cf647e
Fatine BOUJNOUNI <fatine.boujnouni@gmail.Com>
2018-12-31 17:15:05 +0100

    update Texton.m

--- code/Texton.m
+++ code/Texton.m
@@ -1,10 +1,10 @@
 function t = Texton(u)
-U=fft2(u)
-v=abs(U)
-[M,N]=size(u)
-c1=floor((M/2)+1)
-c2=floor((N/2)+1)
-v(c1,c2)= U(c1,c2)
-t=fftshift(ifft2(v))
+U=fft2(u);
+v=abs(U);
+[M,N]=size(u);
+c1=floor((M/2)+1);
+c2=floor((N/2)+1);
+v(c1,c2)= U(c1,c2);
+t=fftshift(ifft2(v));
 
  
