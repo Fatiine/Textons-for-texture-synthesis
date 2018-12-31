2e75ba16942fd94f1b954bcd75fd6f10bb59bdbf
DaoudH <44686864+DaoudH@users.noreply.github.com>
2018-12-31 13:14:19 +0100

    Update Main.m

--- code/Main.m
+++ code/Main.m
@@ -9,7 +9,6 @@ title('Original')
 l=Texton(a);
 l=uint8(l);
 I = double(l);
-I=fftshift(I);
 I=normalize_image(I);
 %Afficher Texton
 figure
@@ -39,6 +38,9 @@ title(" synthesize texture after phase randomization")
 %Afficher La matrice de covariance de a
 imagesc(cov(a));colormap gray;
 
+
+
+
 %Afficher le composant périodique
 [C,s] = per_decomp_smooth(a);
 %C = normalize_image(C);
@@ -54,3 +56,32 @@ imagesc(cov(a));colormap gray;
 figure
 imagesc(C);colormap gray;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%  COLOR IMAGE TEXTON %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%charger une image en couleur
+a=imread('nuages.jpg')
+figure
+imshow(a)
+title('Original')
+
+figure
+alpha=[0.5;0.33;0.17]
+col_texton=Col_Texton(a,alpha)
+imshow(col_texton)
+title('Color Texton')
+
+% Randomization
+Col_texton=fft2(col_texton);
+n=randn(size(col_texton(:,:,1)));
+N=fft2(n);
+
+Col_texton(:,:,1)=abs(Col_texton(:,:,1)).*exp(i*(angle(Col_texton(:,:,1))+angle(N)));
+Col_texton(:,:,2)=abs(Col_texton(:,:,2)).*exp(1i*(angle(Col_texton(:,:,2))+angle(N)));
+Col_texton(:,:,3)=abs(Col_texton(:,:,3)).*exp(i*(angle(Col_texton(:,:,3))+angle(N)));
+
+b=uint8(ifft2(Col_texton));
+figure
+imshow(b)
+title(" Texture synthesis after phase randomization")
