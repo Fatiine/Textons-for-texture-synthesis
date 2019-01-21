145ca26dec4d06692dca94d91025d18669d10ec7
Fatine BOUJNOUNI <fatine.boujnouni@gmail.Com>
2019-01-21 02:00:59 +0100

    update RPN.m and RPN_color.m

--- code/RPN.m
+++ code/RPN.m
@@ -2,6 +2,6 @@ function b = RPN(a)
 A=fft2(a);
 n=randn(size(a));
 N=fft2(n);
-B=abs(A).*exp(i*angle(N));
+B=abs(A).*exp(1i*angle(N));
 b=real(ifft2(B)); 
 end
--- code/RPN_color.m
+++ code/RPN_color.m
@@ -1,9 +1,9 @@
 function b = RPN_color(a)
 A=fft2(a); n=randn(size(a(:,:,1)));
 N=fft2(n); B=zeros(size(a));
-B(:,:,1)=abs(A(:,:,1)).*exp(i*(angle(A(:,:,1))+angle(N)));
-B(:,:,2)=abs(A(:,:,2)).*exp(i*(angle(A(:,:,2))+angle(N)));
-B(:,:,3)=abs(A(:,:,3)).*exp(i*(angle(A(:,:,3))+angle(N)));
+B(:,:,1)=abs(A(:,:,1)).*exp(1i*(angle(A(:,:,1))+angle(N)));
+B(:,:,2)=abs(A(:,:,2)).*exp(1i*(angle(A(:,:,2))+angle(N)));
+B(:,:,3)=abs(A(:,:,3)).*exp(1i*(angle(A(:,:,3))+angle(N)));
 b=real(ifft2(B));
 b=uint8(b);
 end
